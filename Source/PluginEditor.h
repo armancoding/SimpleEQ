ae85e462156ebc4bdecd92b175caac360ccbed67
armancoding <arman.armament@gmail.com>
2022-08-29 22:25:14 -0400

    Implemented Skeleton for custom slider visuals

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,13 +11,39 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotartySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotartySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-        juce::Slider::TextEntryBoxPosition::NoTextBox)
-    {
+    void drawRotarySlider(juce::Graphics&,
+                          int x, int y, int width, int height,
+                          float sliderPosProportional,
+                          float rotaryStartAngle,
+                          float rotaryEndAngle,
+                          juce::Slider&) override {}
+};
 
+struct RotartySliderWithLabels : juce::Slider
+{
+    RotartySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) : 
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+        juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap), suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
     }
+    ~RotartySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    void paint(juce::Graphics& g) override {}
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString() const;
+
+private:
+    LookAndFeel lnf;
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent : juce::Component,
@@ -58,13 +84,13 @@ private:
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
 
-    CustomRotartySlider peakFreqSlider,
-                        peakGainSlider,
-                        peakQualitySlider, 
-                        lowCutFreqSlider, 
-                        highCutFreqSlider,
-                        lowCutSlopeSlider,
-                        highCutSlopeSlider;
+    RotartySliderWithLabels peakFreqSlider,
+                            peakGainSlider,
+                            peakQualitySlider, 
+                            lowCutFreqSlider, 
+                            highCutFreqSlider,
+                            lowCutSlopeSlider,
+                            highCutSlopeSlider;
 
     ResponseCurveComponent responceCurveComponent;
 
